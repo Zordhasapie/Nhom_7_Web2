8425002d73eaffeb3cd6424e56eb948741e2c63f
Zord <quangsang.red@gmail.com>
2023-11-26 23:37:40 +0700

    Thêm section "Địa điểm nổi bật" ở trang chủ, ảnh..

--- Assets/Scripts/app.js
+++ Assets/Scripts/app.js
@@ -1,3 +1,17 @@
+// Onload init
+$(document).ready(function () {
+  // Darkmode Check
+  if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+});
+
 //Components variable
 var nav =
   "" +
