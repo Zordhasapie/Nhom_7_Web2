8e5f184b513d45411181c4ce655506de93577a95
Zord <quangsang.red@gmail.com>
2023-11-30 04:02:34 +0700

    TRULLY NOTHING MUCH

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,1 +1,15 @@
-import { anime } from "./../Assets/Utils/anime";
+$(document).ready(function () {
+  $("#register_link").click(function () {
+    toggleForm("#flogin");
+  });
+  $("#login_link").click(function () {
+    toggleForm("#fregister");
+  });
+
+  function toggleForm(curForm) {
+    var other = curForm == "#fregister" ? "#flogin" : "#fregister";
+    $(curForm).hide(100);
+    $(other).show(500);
+  }
+  console.log("ready");
+});
