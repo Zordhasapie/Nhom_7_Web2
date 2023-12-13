84cd55bd6791a795758bcc1819d134578ee0b789
Zord <quangsang.red@gmail.com>
2023-12-14 02:55:38 +0700

    Added more stuff

--- Auth/auth.js
+++ Auth/auth.js
@@ -16,10 +16,53 @@ $(document).ready(function () {
   // Regex for Firstname Lastname
   var nameRegex =
     /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+
+  // Regex for username
   var usernameRegex = /^[a-zA-Z0-9]+$/;
+  // Regex for mail
   var emailRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^__`{|}-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  // Regex for password
   var passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&**])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+
+  // Validate login form
+  var loginvalid = {
+    username: false,
+    password: false,
+  };
+  $("#flogin input").on({
+    "blur change": function () {
+      let val = $(this).val();
+      if (val == "") {
+        invalidHandler($(this), "Thông tin này là bắt buộc.");
+        loginvalid[$(this).attr("name")] = false;
+      }
+    },
+    focus: function () {
+      $(this).parent().find(".invalid-feedback").text("").hide();
+      loginvalid[$(this).attr("name")] = true;
+    },
+  });
+
+  $("#flogin").change(function () {
+    var isValid = true;
+    $.each(loginvalid, function (key, val) {
+      if (!val) isValid = false;
+      console.log(key, val);
+    });
+    console.log(isValid);
+    if (isValid) $("#login_btn").prop("disabled", false);
+    else $("#login_btn").prop("disabled", true);
+  });
+  // End validate login form
+  // Login btn handler
+  $("#login_btn").click(function (e) {
+    if ($(this).attr("disabled") == true) e.preventDefault();
+    else window.location.href = "../index.html";
+  });
+
+  // Validate register form
+  // All fields are invalid by default
   var Input = {
     Firstname: false,
     Lastname: false,
@@ -29,7 +72,6 @@ $(document).ready(function () {
     Email: false,
   };
 
-  // Validate register form
   $("#fregister input")
     .slice(0, 2)
     .on({
@@ -105,6 +147,12 @@ $(document).ready(function () {
     if (isValid && $("#tos").is(":checked"))
       $("#register_btn").prop("disabled", false);
     else $("#register_btn").prop("disabled", true);
+  });
+  // end validate register form
+  // Register btn handler
+  $("#register_btn").click(function (e) {
+    if ($(this).attr("disabled") == true) e.preventDefault();
+    else window.location.href = "../index.html";
   });
 });
 
